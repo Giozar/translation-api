a69f93c153df944e4f48a43896e7024894a41a71
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-01-23 13:07:10 -0600

    Datos de la base de datos añadidos localmente e implementación de traducción de datos con deepl

--- src/controllers/deepl.controller.js
+++ src/controllers/deepl.controller.js
@@ -1,19 +1,92 @@
 import * as deepl from "deepl-node";
 import { deeplAPIKEY } from "../../config.js";
+import * as fs from "fs";
+import categories from "../db/desumex.categories.json" assert { type: "json" };
+import tools from "../db/desumex.tools.json" assert { type: "json" };
+
 const authKey = deeplAPIKEY;
 const translator = new deepl.Translator(authKey);
 
-async function deeplTranslate(req, res){
+function saveTranslation(data) {
+    const fileName = "tools.json";
+    const filePath = "src/db/translated/" + fileName; 
+    const jsonData = JSON.stringify(data);
 
-    const languageToTranslate = "es";  
-
-    const inputText = 'Hellos, world';
-
-    const result = await translator.translateText( inputText, null, languageToTranslate);
-    res.json({
-        message: inputText,
-        translation: result.text,
-    })
+    fs.access( filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            //Si el archivo no existe, crea el archivo y lo escribe
+            fs.writeFile(filePath, jsonData, "utf-8" , (err) => {
+                if (err) throw err;
+                console.log("File created and first object written");
+            });
+        } else {
+            fs.appendFile(filePath, jsonData, "utf-8", (err) => {
+                if (err) throw err;
+                console.log("Data writed");
+            });
+        }
+    });
 }
+
+async function dataTranslate() {
+    const newData = [];
+    const dataToTranslate = tools;
+    const languageToTranslate = "es";
+
+    // const inputText = 'Hellos, world';
+    // const result = await translator.translateText( inputText, null, languageToTranslate);
+    // res.json({
+    //     message: inputText,
+    //     translation: result.text,
+    // });
+    let sizes = 0;
+
+    await Promise.all(dataToTranslate.map(async (data) => {
+        for (const key in data) {
+            if (Object.hasOwnProperty.call(data, key)) {
+                const element = String(data[key]);
+                if (
+                    (
+                        key !== "_id" 
+                        || key !== "Referencia" 
+                        || key !== "Modelo" 
+                        || key !== "name" 
+                        || key !== "categoryId" 
+                        || key !== "subcategoryId"
+                        || key !== "subSubcategoryId"
+                        || key !== "subSubSubcategoryId"
+                        || key !== "subSubSubcategoryId"
+                        || key !== "image"
+                        || key !== "pathName"
+                        || key !== "href"
+                    )
+                && element.length > 0 && element !== null) {
+                    // const result = await translator.translateText(element, null, languageToTranslate);
+                    // data[key] = result.text;
+
+                    sizes += element.length;
+                }
+            }
+        }
+
+        
+        newData.push(data);
+    }));
+    
+    console.log(sizes);
+    
+    return newData;
+}
+
+async function deeplTranslate() {
+    const data = await dataTranslate();
+    // console.log(data);
+    // await saveTranslation( data );
+}
+
+
+
+
+
 
 export default deeplTranslate;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app from "./app.js";
+import deeplTranslate from "./controllers/deepl.controller.js";
 
 app.listen(3000);
+
+deeplTranslate();
 
 console.log("server on port ", 3000);
